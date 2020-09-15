ac469f5a44fd1d98d19e92a37904f328f6a86537
Teknokz <60339592+Teknokz@users.noreply.github.com>
2020-09-15 13:34:48 +0200

    Övning

---
+++ Saving-Grejs/FPS.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Saving_Grejs
+{
+    public class FPS
+    {
+
+        public int HP;
+
+        public int x;
+
+        public int y;
+
+        public int z;
+
+    }
+}
--- Saving-Grejs/Program.cs
+++ Saving-Grejs/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
-
+using System.Xml.Serialization;
+using System.IO;
 namespace Saving_Grejs
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+        //    FPS myFPS = new FPS();
+        //    myFPS.HP = 100;
+        //    myFPS.x = 0;
+        //    myFPS.y = 13;
+        //    myFPS.z = 12;
+
+            XmlSerializer serializer = new XmlSerializer(typeof(FPS));
+
+            FileStream file = File.Open("FPS.xml", FileMode.OpenOrCreate);
+
+            FPS myFPS = (FPS) serializer.Deserialize(file, myFPS);
+
+            file.Close();
+
         }
     }
 }
