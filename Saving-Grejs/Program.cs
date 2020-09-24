9002a5d32279fe6dace02a320d86e6fb94a69bf1
Teknokz <60339592+Teknokz@users.noreply.github.com>
2020-09-24 16:22:39 +0200

    Går nu att ändra på värderna i klassen

--- Saving-Grejs/Program.cs
+++ Saving-Grejs/Program.cs
@@ -1,25 +1,74 @@
 ﻿using System;
 using System.Xml.Serialization;
 using System.IO;
+using System.Xml;
 namespace Saving_Grejs
 {
     class Program
     {
         static void Main(string[] args)
         {
-        //    FPS myFPS = new FPS();
-        //    myFPS.HP = 100;
-        //    myFPS.x = 0;
-        //    myFPS.y = 13;
-        //    myFPS.z = 12;
+       FPS myFPS = new FPS();
+       myFPS.HP = 100;
+       myFPS.x = 0;
+       myFPS.y = 13;
+       myFPS.z = 12;
 
-            XmlSerializer serializer = new XmlSerializer(typeof(FPS));
+            System.Console.WriteLine("Hej och välkommen, vill du ändra värdena i myFPS?");
+            System.Console.WriteLine("ja eller nej");
+            
+            string knas = Console.ReadLine();
 
-            FileStream file = File.Open("FPS.xml", FileMode.OpenOrCreate);
+            if(knas == "ja" || knas == "Ja")
+            {
+        
+                System.Console.WriteLine("Vad vill du ändra HP till?");
+                
 
-            FPS myFPS = (FPS) serializer.Deserialize(file, myFPS);
+                string input1 = Console.ReadLine();
+                
+                int.TryParse(input1, out myFPS.HP);
+
+            
+                System.Console.WriteLine("Vad vill du ändra x till?");
+                
+
+                string input2 = Console.ReadLine();
+                
+                int.TryParse(input2, out myFPS.x);
+
+                System.Console.WriteLine("Vad vill du ändra y till?");
+                
+
+                string input3 = Console.ReadLine();
+                
+                int.TryParse(input3, out myFPS.y);
+
+                System.Console.WriteLine("Vad vill du ändra z till?");
+                
+
+                string input4 = Console.ReadLine();
+                
+                int.TryParse(input4, out myFPS.z);
+
+                System.Console.WriteLine("Sparar...");
+
+
+            XmlSerializer Serializer = new XmlSerializer(typeof(FPS));
+
+
+            FileStream file = File.Open(@"FPS.xml", FileMode.OpenOrCreate);
+
+            Serializer.Serialize(file, myFPS);
 
             file.Close();
+            }
+            else
+            {
+                System.Console.WriteLine("Godnatt");
+            }
+
+
 
         }
     }
